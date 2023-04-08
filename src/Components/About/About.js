f845ded31fb145d45b76f68ba93d8de617599248
riyadmahmudkhan <mdriyadhossain356@gmail.com>
2023-04-08 14:52:04 +0600

    m

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,7 +3,10 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h3>Hi, Iam Riyad</h3>
+            <h1>Hi, Iam Riyad Hossain.</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores harum nostrum quasi, recusandae dicta aspernatur. Ullam sequi odio veniam beatae cumque culpa nulla possimus magni commodi, error suscipit quas accusamus eaque ex fuga, maxime deleniti ipsam consectetur. A beatae voluptatum, culpa pariatur quo tenetur delectus similique accusantium enim impedit consequuntur. Perferendis ratione cupiditate fuga voluptate perspiciatis natus provident libero laboriosam, cumque quidem accusantium. Dolores cupiditate eaque repellendus fugiat ullam dicta dolore saepe? Inventore expedita repellendus aliquid, maxime magnam voluptatum in quia mollitia voluptas sint facere dolores necessitatibus? Velit delectus, quas dolores at sint optio, quaerat quis consequatur, mollitia officiis unde quam porro deserunt reiciendis sed voluptate ullam incidunt vero? Corrupti sed quasi magni ea iste aliquid, ex enim vel, est vero voluptatibus totam consequuntur blanditiis dignissimos exercitationem maiores dolor quod! Recusandae, facilis explicabo fugiat at nostrum beatae aliquam deleniti sapiente autem, saepe, nemo dolorem. Dolorem nesciunt officiis dicta nostrum magnam maxime quam laboriosam quod ducimus quos, omnis sit unde, nemo ex perspiciatis! Tempore, tempora corporis nostrum aspernatur nobis voluptas earum dicta quod officiis voluptatum sint, delectus dolorum itaque quasi qui maxime consequatur blanditiis. Voluptas libero laborum beatae sunt maxime, ad non numquam nostrum maiores quas deleniti dolores id? Non, corporis.
+            </p>
         </div>
     );
 };
